dad6bf11a5ce0097bc4974b2b1b8944106e4c2dd
Cameron Craddock <cameorn.craddock@gmail.com>
2016-09-10 00:53:39 +0000

    seems to run all of the way through now

--- run.py
+++ run.py
@@ -5,17 +5,33 @@ import sys
 import urllib
 import pandas as pd
 import patsy
-import commands
 import shutil
 from create_flame_model_files import create_flame_model_files
 import glob
 import json
 import numpy as np
+import subprocess
 #import nibabel
 #import numpy
 #from glob import glob
 
 __version__ = 0.1
+
+
+def run(command, env={}):
+    merged_env = os.environ
+    merged_env.update(env)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT, shell=True,
+                               env=merged_env)
+    while True:
+        line = process.stdout.readline()
+        line = line.encode('utf-8')[:-1]
+        print(line)
+        if line == '' and process.poll() != None:
+            break
+    if process.returncode != 0:
+        raise Exception("Non zero return code: %d"%process.returncode)
  
 parser = argparse.ArgumentParser(description='ABIDE Group Analysis Runner')
 
@@ -66,16 +82,19 @@ if not os.path.isdir(working_dir):
     print("Could not find bids directory %s"%(bids_dir))
     sys.exit(1)
 
+print ("\n")
 print ("## Running randomize pipeline with parameters:")
 print ("Output directory: %s"%(bids_dir))
 print ("Output directory: %s"%(output_dir))
 print ("Working directory: %s"%(working_dir))
 print ("Pheno file: %s"%(args.model_file))
-print ("\n\n")
+print ("\n")
 
 # read in the pheno file
 pheno_df=pd.read_csv(os.path.join(bids_dir, 'participants.tsv'),sep='\t')
-#print pheno_df.head()
+
+# remove rows that have empty elements
+pheno_df=pheno_df.dropna()
 
 # go through data, verify that we can find a corresponding entry in
 # the pheno file, and keep track of the indices so that we can 
@@ -89,8 +108,10 @@ for root, dirs, files in os.walk(bids_dir):
         f_chunks = (filename.split(".")[0]).split("_")
         # make a dictionary from the key-value chunks
         f_dict = {chunk.split("-")[0]:"-".join(chunk.split("-")[1:]) for chunk in f_chunks[:-1]}
-        file_list.append(os.path.join(root,filename))
-        pheno_key_list.append(pheno_df[(pheno_df["participant_id"]=="-".join(["sub",f_dict["sub"]]))].index.tolist()[0])
+        pheno_flags=pheno_df["participant_id"]==("-".join(["sub",f_dict["sub"]]))
+        if pheno_flags.any():
+            pheno_key_list.append(np.where(pheno_flags)[0][0])
+            file_list.append(os.path.join(root,filename))
 
 # merge the fines into 4D
 merge_input = " ".join(file_list)
@@ -103,10 +124,10 @@ if not os.path.isfile(merge_output):
     merge_string = "fslmerge -t %s %s" % (merge_output, merge_input)
     
     # MERGE the outputs
-    res=commands.getoutput(merge_string)
-    if len(res)>0 and res[0] != 0:
-        print "[!] FSL Merge failed for output: %s" % res[1]
-        print res
+    try:
+        run(merge_string)
+    except:
+        print "[!] FSL Merge failed for output" 
         raise
 else:
     print "%s already exists, skipping merge"%(merge_output)
@@ -119,20 +140,15 @@ if not os.path.isfile(merge_mask_output):
     merge_mask_string = "fslmaths %s -abs -Tmin -bin %s" % (merge_output, merge_mask_output)
     
     # CREATE A MASK of the merged file
-    res=commands.getoutput(merge_mask_string)
-    if len(res)>0 and res[0] != 0:
-        print "[!] FSL Mask failed for output: %s" % res[1]
-        print res
+    try:
+        run(merge_mask_string)
+    except:
+        print "[!] FSL Mask failed for output" 
         raise
 
 #### now create the design.mat file
-
 # reduce to the rows that we are using, and reorder to match the file list
 pheno_df=pheno_df.iloc[pheno_key_list,:]
-
-print file_list[0:5]
-print pheno_df.head()
-
 
 # load in the model 
 with open(model_file) as model_fd:    
@@ -147,21 +163,19 @@ pheno_df=pheno_df[incols]
 for df_ndx in pheno_df.columns:
     if np.issubdtype(pheno_df[df_ndx].dtype,np.number):
         pheno_df[df_ndx]-=pheno_df[df_ndx].mean()
-    print df_ndx
 
 # use patsy to create the design matrix
-design=patsy.dmatrix(model_dict["model"],pheno_df)
+design=patsy.dmatrix(model_dict["model"],pheno_df,NA_action='raise')
 column_names = design.design_info.column_names
-print column_names
 
 # create contrasts
 contrast_dict={}
+num_contrasts=0
 for k in model_dict["contrasts"]:
-    print k
+    num_contrasts+=1
     contrast_dict[k]=design.design_info.linear_constraint(k.encode('ascii')).coefs[0]
 
 num_subjects=len(file_list)
-
 mat_file, grp_file, con_file, fts_file = create_flame_model_files(design, \
     column_names, contrast_dict, None, [], None, [1] * num_subjects, "Treatment", \
     "repro_pipe_model", [], working_dir)
@@ -174,35 +188,35 @@ print "writing results to %s"%(rando_out_prefix)
 rando_string="randomise -i %s -o %s -d %s -t %s -m %s -n 10 -D -T"%(merge_output, 
     rando_out_prefix, mat_file, con_file, merge_mask_output)
 
-res=commands.getoutput(rando_string)
-if len(res)>0 and res[0] != 0:
+try:
+    run(rando_string)
+except:
     print "[!] FSL randomise failed."
-    print res
     raise
 
 # ## now do the clustering stuff
 # fslmaths grot_tfce_corrp_tstat1 -thr 0.95 -bin -mul grot_tstat1 grot_thresh_tstat1
 
-for i in range(1,3):
-    thresh_string="fslmaths %s_tfce_corrp_tstat%d -thr 0.95 -bin -mul %s_tstat%d %s_thresh_tstat1"%(
-        rando_out_prefix,i,rando_out_prefix,i,rando_out_prefix)
+for i in range(1,num_contrasts+1):
+    thresh_string="fslmaths %s_tfce_corrp_tstat%d -thr 0.95 -bin -mul %s_tstat%d %s_thresh_tstat%d"%(
+        rando_out_prefix,i,rando_out_prefix,i,rando_out_prefix,i)
 
-    res=commands.getoutput(thresh_string)
-    if len(res)>0 and res[0] != 0:
+    try:
+        print "Threshold tstat%d"%(i)
+        run(thresh_string)
+    except:
         print "[!] FSL fslmaths thresh failed."
-        print res
         raise
 
-
 # clust_string=cluster --in=grot_thresh_tstat1 --thresh=0.0001 --oindex=grot_cluster_index --olmax=grot_lmax.txt --osize=grot_cluster_size
-for i in range(1,3):
+for i in range(1,num_contrasts+1):
     clust_string="cluster --in=%s_thresh_tstat%d --thresh=0.0001 --oindex=%s_cluster_index --olmax=%s_lmax.txt --osize=%s_cluster_size"%(
         rando_out_prefix, i, rando_out_prefix, rando_out_prefix, rando_out_prefix)
     
-    res=commands.getoutput(clust_string)
-    if len(res)>0 and res[0] != 0:
+    try:
+        run(clust_string)
+    except:
         print "[!] FSL cluster failed."
-        print res
         raise
 
 ## copy results to the output directory
